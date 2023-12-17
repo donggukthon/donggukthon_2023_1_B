28af2915139981fc11877b8bef7cfba11e39b0b5
nykoh2001 <nykoh2001@gmail.com>
2023-12-18 03:35:41 +0900

    feat: invitation entity

---
+++ src/main/java/flirting/demo/entity/Invitation.java
@@ -0,0 +1,39 @@
+package flirting.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Invitation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "invitation_id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "is_accepted")
+    @ColumnDefault("false")
+    private boolean isAccepted;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private Member receiver;
+
+    public void updateIsAccepted(boolean isAccepted) {
+        this.isAccepted = isAccepted;
+    }
+
+    @Builder
+    public Invitation(boolean isAccepted, Member sender, Member receiver) {
+        this.isAccepted = isAccepted;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+}
