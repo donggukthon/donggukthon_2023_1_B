69b477e7e3da0ceb5b1996f5a8d0cb822fad7474
Seongjoon-K <jobcho6320@naver.com>
2023-12-18 00:47:51 +0900

    initial setting

---
+++ src/test/java/flirting/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package flirting.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
