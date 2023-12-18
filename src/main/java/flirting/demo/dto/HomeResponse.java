1f242817578d3558b893d213c180a978755ab9b0
nykoh2001 <nykoh2001@gmail.com>
2023-12-18 15:16:35 +0900

    feat: get home api

---
+++ src/main/java/flirting/demo/dto/HomeResponse.java
@@ -0,0 +1,51 @@
+package flirting.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import flirting.demo.entity.Invitation;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class HomeResponse {
+    private List<InvitationData> invitationList = new ArrayList<>();
+
+    @Builder
+    public HomeResponse (List<Invitation> invitations) {
+        invitations.forEach(invitation -> {
+            this.invitationList.add(InvitationData.builder()
+                            .inviterId(invitation.getId())
+                            .inviterName(invitation.getSender().getUsername())
+                            .groupId(invitation.getGroup().getId())
+                            .groupName(invitation.getGroup().getName())
+                    .build());
+        });
+    }
+
+    @Getter
+    public static class InvitationData {
+        @JsonProperty("inviterId")
+        private Long inviterId;
+
+        @JsonProperty("inviterName")
+        private String inviterName;
+
+        @JsonProperty("groupId")
+        private Long groupId;
+
+        @JsonProperty("groupName")
+        private String groupName;
+
+        @Builder
+        public InvitationData(Long inviterId, String inviterName, Long groupId, String groupName){
+            this.inviterId = inviterId;
+            this.inviterName = inviterName;
+            this.groupId = groupId;
+            this.groupName = groupName;
+        }
+
+
+    }
+}
