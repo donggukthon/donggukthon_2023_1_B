660fbe5bc9d45cbdf91648f156fe07a42a873ba3
nykoh2001 <nykoh2001@gmail.com>
2023-12-18 21:37:04 +0900

    feat: 나를 제외한 그룹 멤버 조회

--- src/main/java/flirting/demo/repository/MemberRepository.java
+++ src/main/java/flirting/demo/repository/MemberRepository.java
@@ -2,8 +2,15 @@ package flirting.demo.repository;
 
 import flirting.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query("select m from Member m join m.groups g where g.id = :groupId and m.id <> :memberId")
+    public List<Member> getGroupMembersExceptMe(@Param("memberId") Long memberId,
+                                                @Param("groupId") Long groupId);
 }
