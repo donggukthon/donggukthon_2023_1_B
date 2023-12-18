28e4b2da196d0ca4fb80dfb3877f44676d7812d2
nykoh2001 <nykoh2001@gmail.com>
2023-12-18 12:38:11 +0900

    feat: question repository

---
+++ src/main/java/flirting/demo/repository/QuestionRepository.java
@@ -0,0 +1,10 @@
+package flirting.demo.repository;
+
+import flirting.demo.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+}
