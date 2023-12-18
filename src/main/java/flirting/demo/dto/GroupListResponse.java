d3b54c7d44fdd10202ff035f197943037bf2bdda
nykoh2001 <nykoh2001@gmail.com>
2023-12-18 16:07:07 +0900

    feat: get group list api

---
+++ src/main/java/flirting/demo/dto/GroupListResponse.java
@@ -0,0 +1,35 @@
+package flirting.demo.dto;
+
+import flirting.demo.entity.Group;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class GroupListResponse {
+
+    List<GroupListData> groupList = new ArrayList<>();
+
+    public GroupListResponse(List<Group> groups) {
+        groups.forEach(group -> {
+            this.groupList.add(GroupListData.builder()
+                            .groupId(group.getId())
+                            .groupName(group.getName())
+                    .build());
+        });
+    }
+
+    @Getter
+    public static class GroupListData {
+        Long groupId;
+        String groupName;
+
+        @Builder
+        public GroupListData (Long groupId, String groupName) {
+            this.groupId = groupId;
+            this.groupName = groupName;
+        }
+    }
+}
