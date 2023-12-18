3977b1845095a3a90aaad7592d7056d0f8cd02d9
Seongjoon-K <jobcho6320@naver.com>
2023-12-18 14:28:50 +0900

    feat: health check 메소드 추가

--- src/main/java/flirting/demo/controller/MemberController.java
+++ src/main/java/flirting/demo/controller/MemberController.java
@@ -1,4 +1,28 @@
 package flirting.demo.controller;
 
+import flirting.demo.common.ApiStatus;
+import flirting.demo.common.StatusCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+
+    @GetMapping
+    private ResponseEntity<ApiStatus> healthCheck() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        return new ResponseEntity<>(
+                new ApiStatus(StatusCode.OK, "요청 확인"),
+                httpHeaders, HttpStatus.OK);
+
+    }
+
 }
