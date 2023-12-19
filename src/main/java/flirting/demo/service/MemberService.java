b9f253ffbb26aab902a896ae778a42d6376f9280
nykoh2001 <nykoh2001@gmail.com>
2023-12-20 04:34:45 +0900

    feat: get setting info

--- src/main/java/flirting/demo/service/MemberService.java
+++ src/main/java/flirting/demo/service/MemberService.java
@@ -1,4 +1,16 @@
 package flirting.demo.service;
 
+import flirting.demo.entity.Member;
+import flirting.demo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member getMemberById(Long memberId) {
+        return memberRepository.getReferenceById(memberId);
+    }
 }
