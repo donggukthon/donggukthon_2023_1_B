2f8d282f0e3deca72aa41a50933e35ab42d44264
nykoh2001 <nykoh2001@gmail.com>
2023-12-20 14:36:50 +0900

    fix: try-catch delete

--- src/main/java/flirting/demo/service/GroupService.java
+++ src/main/java/flirting/demo/service/GroupService.java
@@ -18,7 +18,6 @@ public class GroupService {
 
     public Group create(GroupCreateRequest groupCreateRequest) {
 //        System.out.println("group name: " + groupCreateRequest.getName());
-        try {
             Group group = groupRepository.save(
                     Group.builder()
                             .name(groupCreateRequest.getName())
@@ -33,19 +32,13 @@ public class GroupService {
             memberRepository.save(member);
 
             return group;
-        }
-        catch (RuntimeException e) {
-            throw new RuntimeException("그룹 생성 실패");
-        }
+
 
     }
 
     public List<Group> getGroups(Long memberId) {
-        try {
             List<Group> groups = groupRepository.getGroupsByMemberId(memberId);
             return groups;
-        }catch (RuntimeException e) {
-            throw new RuntimeException("그룹 목록 조회 실패");
-        }
+
     }
 }
