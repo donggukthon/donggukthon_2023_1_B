e55419db7ec37a1c292773cba3aa607c80406ecb
Seongjoon-K <jobcho6320@naver.com>
2023-12-20 09:24:10 +0900

    feat: MemberResponse 추가

--- src/main/java/flirting/demo/dto/MemberResponse.java
+++
@@ -1,7 +0,0 @@
-package flirting.demo.dto;
-
-import jakarta.persistence.Entity;
-import lombok.Getter;
-
-public class MemberResponse {
-}
---
+++ src/main/java/flirting/demo/dto/response/MemberResponse.java
@@ -0,0 +1,37 @@
+package flirting.demo.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import flirting.demo.entity.Member;
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class MemberResponse {
+
+    @JsonProperty("sub")
+    private String oauthId;
+
+    @JsonProperty("name")
+    private String username;
+    private String email;
+
+    @Builder
+    public MemberResponse(String sub, String username, String picture) {
+
+        this.oauthId = oauthId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .oauthId(oauthId)
+                .username(username)
+                .email(email)
+                .build();
+    }
+}
