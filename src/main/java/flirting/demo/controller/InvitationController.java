554e995743fda2cf7a9a1730c6ee961b94d54ef0
nykoh2001 <nykoh2001@gmail.com>
2023-12-20 11:39:31 +0900

    fix: get invitation

--- src/main/java/flirting/demo/controller/InvitationController.java
+++ src/main/java/flirting/demo/controller/InvitationController.java
@@ -6,8 +6,10 @@ import flirting.demo.common.StatusCode;
 import flirting.demo.dto.request.InvitationAcceptRequest;
 import flirting.demo.dto.response.InvitationAcceptResponse;
 import flirting.demo.dto.response.InvitationResponse;
+import flirting.demo.dto.response.MemberResponse;
 import flirting.demo.entity.Invitation;
 import flirting.demo.service.InvitationService;
+import flirting.demo.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,14 +21,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/invitation")
 public class InvitationController {
     private final InvitationService invitationService;
+    private final OAuthService oAuthService;
 
     @GetMapping(value = "/{inviterId}/{groupId}", produces = "application/json")
     public ResponseEntity<Object> getInvitationList(@PathVariable("inviterId") Long inviterId, @PathVariable("groupId") Long groupId) {
         // Todo: member id, group id로 초대장 조회
-        Long receiverId = 1L;
+
         HttpHeaders httpheaders = new HttpHeaders();
         try {
-            Invitation invitation = invitationService.createInvitation(inviterId, receiverId, groupId);
+            Invitation invitation = invitationService.createInvitation(inviterId, groupId);
             Long memberCnt = invitationService.getMemberCnt(groupId);
             InvitationResponse invitationResponse = InvitationResponse.builder()
                     .invitation(invitation)
@@ -51,14 +54,11 @@ public class InvitationController {
     public ResponseEntity<Object> accept(@RequestBody InvitationAcceptRequest invitationAcceptRequest){
         HttpHeaders httpHeaders = new HttpHeaders();
         try {
-            Invitation invitation = invitationService.acceptInvitation(invitationAcceptRequest);
+            InvitationAcceptResponse invitation = invitationService.acceptInvitation(invitationAcceptRequest);
             return new ResponseEntity<>(
                     new ResponseData(
                             new ApiStatus(StatusCode.OK, "초대 수락 성공"),
-                            InvitationAcceptResponse.builder()
-                                    .memberName(invitation.getReceiver().getUsername())
-                                    .groupName(invitation.getGroup().getName())
-                                    .build()
+                            invitation
                     ),
                     httpHeaders, HttpStatus.OK
             );
--- src/main/java/flirting/demo/service/InvitationService.java
+++ src/main/java/flirting/demo/service/InvitationService.java
@@ -1,6 +1,7 @@
 package flirting.demo.service;
 
 import flirting.demo.dto.request.InvitationAcceptRequest;
+import flirting.demo.dto.response.InvitationAcceptResponse;
 import flirting.demo.entity.Group;
 import flirting.demo.entity.Invitation;
 import flirting.demo.entity.Member;
@@ -20,17 +21,15 @@ public class InvitationService {
     private final InvitationRepository invitationRepository;
     private final GroupRepository groupRepository;
 
-    public Invitation createInvitation(Long inviterId, Long receiverId, Long groupId) {
+    public Invitation createInvitation(Long inviterId, Long groupId) {
         try {
             // Todo: get memer id -> invitation 생성 후 저장 ...
             // Todo: 동일 member id, group id인 멤버 이미 존재하면 오류 반환
             Group group = groupRepository.getReferenceById(groupId);
             Member inviter = memberRepository.getReferenceById(inviterId);
-            Member receiver = memberRepository.getReferenceById(receiverId);
 
             Invitation invitation = Invitation.builder()
                     .sender(inviter)
-                    .receiver(receiver)
                     .group(group)
                     .build();
 
@@ -45,21 +44,26 @@ public class InvitationService {
         return  memberRepository.getMemberCnt(groupId);
     }
 
-    public Invitation acceptInvitation(InvitationAcceptRequest invitationAcceptRequest) {
+    public InvitationAcceptResponse acceptInvitation(InvitationAcceptRequest invitationAcceptRequest) {
         try {
             Long receiverId = invitationAcceptRequest.getMemberId();
             Long groupId = invitationAcceptRequest.getGroupId();
+            Long inviterId = invitationAcceptRequest.getInviterId();
             // Todo: groupId, memberId로 조회했을 때 두개 이상이면 예외 처리 로직 추갸
-            Invitation invitation = invitationRepository.getInvitationByReceiverAndGroup(receiverId, groupId)
-                    .orElseThrow(() -> new RuntimeException("조건에 맞는 초대장이 없습니다."));
+//            Invitation invitation = invitationRepository.getInvitationByReceiverAndGroup(receiverId, groupId)
+//                    .orElseThrow(() -> new RuntimeException("조건에 맞는 초대장이 없습니다."));
 
             // 초대장 수락 상태로 변경
-            if (invitation.isAccepted() == false) {
-                invitation.updateIsAccepted(true);
-            }
+//            if (invitation.isAccepted() == false) {
+//                invitation.updateIsAccepted(true);
+//            }
 
-            Member member = memberRepository.getReferenceById(receiverId);
+            Member receiver = memberRepository.getReferenceById(receiverId);
             Group group = groupRepository.getReferenceById(groupId);
+            Member inviter = memberRepository.getReferenceById(inviterId);
+
+            inviter.updateSnowflake(+20);
+            memberRepository.save(inviter);
 
             // 예외 처리
             List<Group> alreayExistGroups = groupRepository.getGroupsByMemberId(receiverId);
@@ -67,13 +71,17 @@ public class InvitationService {
             if (duplicateGroup.isEmpty()) {
 
                 // group에 member 추가
-                member.getGroups().add(group);
-                group.getMembers().add(member);
+                receiver.getGroups().add(group);
+                group.getMembers().add(receiver);
 
-                memberRepository.save(member);
+                memberRepository.save(receiver);
                 groupRepository.save(group);
 
-                Invitation _invitation = invitationRepository.save(invitation);
+                InvitationAcceptResponse _invitation = InvitationAcceptResponse
+                        .builder()
+                        .memberName(receiver.getUsername())
+                        .groupName(group.getName())
+                        .build();
 
                 return _invitation;
             }else throw new RuntimeException("이미 가입된 그룹입니다.");
