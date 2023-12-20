b7b0966b6eb9b9aa20eff9b37ae61e87df83b1ce
Seongjoon-K <jobcho6320@naver.com>
2023-12-20 09:22:43 +0900

    feat: 구글 토큰 기반 email, oauthId 필드 추가 및 password 필드 제거

--- src/main/java/flirting/demo/entity/Member.java
+++ src/main/java/flirting/demo/entity/Member.java
@@ -1,17 +1,21 @@
 package flirting.demo.entity;
 
+import flirting.demo.dto.response.MemberResponse;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "members")
 public class Member {
 
@@ -23,8 +27,11 @@ public class Member {
     @Column(length = 10, name = "username", nullable = false)
     private String username;
 
-    @Column(length = 500, nullable = false)
-    private String password;
+    @Column(length = 50)
+    private String email;
+
+    @Column(name = "oauth_id",length = 1000)
+    private String oauthId;
 
     @Column(length = 50, nullable = false)
     private int snowflake;
@@ -35,5 +42,7 @@ public class Member {
     public void updateSnowflake(int amount) {
         this.snowflake += amount;
     }
+
+
 
 }
