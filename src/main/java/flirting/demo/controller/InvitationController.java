4b836e5550dd223721816827a54627794d862452
nykoh2001 <nykoh2001@gmail.com>
2023-12-20 10:35:18 +0900

    fix: get invitation

--- src/main/java/flirting/demo/controller/InvitationController.java
+++ src/main/java/flirting/demo/controller/InvitationController.java
@@ -3,11 +3,10 @@ package flirting.demo.controller;
 import flirting.demo.common.ApiStatus;
 import flirting.demo.common.ResponseData;
 import flirting.demo.common.StatusCode;
-import flirting.demo.dto.response.InvitationListResponse;
 import flirting.demo.dto.request.InvitationAcceptRequest;
 import flirting.demo.dto.response.InvitationAcceptResponse;
+import flirting.demo.dto.response.InvitationResponse;
 import flirting.demo.entity.Invitation;
-import flirting.demo.entity.Member;
 import flirting.demo.service.InvitationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -15,25 +14,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/invitation")
 public class InvitationController {
     private final InvitationService invitationService;
 
-    @GetMapping(value = "/{memberId}/{groupId}", produces = "application/json")
-    public ResponseEntity<Object> getInvitationList(@PathVariable("memberId") Long memberId, @PathVariable("groupId") Long groupId) {
+    @GetMapping(value = "/{inviterId}/{groupId}", produces = "application/json")
+    public ResponseEntity<Object> getInvitationList(@PathVariable("inviterId") Long inviterId, @PathVariable("groupId") Long groupId) {
         // Todo: member id, group id로 초대장 조회
+        Long receiverId = 1L;
         HttpHeaders httpheaders = new HttpHeaders();
         try {
-            List<Invitation> invitations = invitationService.getInvitationList(memberId);
-            InvitationListResponse homeResponse = new InvitationListResponse(invitations);
-            return new ResponseEntity<>(new ResponseData(
-                    new ApiStatus(StatusCode.OK, "초대장 조회 성공"),
-                    homeResponse
-            ), httpheaders, HttpStatus.OK);
+            Invitation invitation = invitationService.createInvitation(inviterId, receiverId, groupId);
+            Long memberCnt = invitationService.getMemberCnt(groupId);
+            InvitationResponse invitationResponse = InvitationResponse.builder()
+                    .invitation(invitation)
+                    .memberCnt(memberCnt)
+                    .build();
+
+            return new ResponseEntity<>(
+                    new ResponseData(
+                            new ApiStatus(StatusCode.OK, "초대장 조회 성공"),
+                            invitationResponse
+                    ),
+                    httpheaders, HttpStatus.OK
+            );
         }catch (RuntimeException e) {
             return new ResponseEntity<>(
                     new ApiStatus(StatusCode.INTERNAL_SERVER_ERROR, e.getMessage()),
