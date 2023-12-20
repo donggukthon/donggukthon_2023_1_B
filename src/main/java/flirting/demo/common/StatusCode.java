6122aca663eb99cdc9ff12efbbf513b75b5521e7
nykoh2001 <nykoh2001@gmail.com>
2023-12-20 09:14:54 +0900

    chore: custom status 추가

--- src/main/java/flirting/demo/common/StatusCode.java
+++ src/main/java/flirting/demo/common/StatusCode.java
@@ -15,7 +15,8 @@ public enum StatusCode {
     NO_OTHER_MEMBERS_IN_GROUP(HttpStatus.INTERNAL_SERVER_ERROR, "N502", "There are no other member in group"),
     MYSELF_IN_OPTIONS(HttpStatus.INTERNAL_SERVER_ERROR, "N503", "There is user in vote options"),
     NO_SELECTED_QUESTION(HttpStatus.INTERNAL_SERVER_ERROR, "N504", "There is no selected questions"),
-    NO_SELECTED_VOTE(HttpStatus.INTERNAL_SERVER_ERROR, "N505", "There is no votes selected by question")
+    NO_SELECTED_VOTE(HttpStatus.INTERNAL_SERVER_ERROR, "N505", "There is no votes selected by question"),
+    SNOWFLAKE_NOT_ENOUGH(HttpStatus.INTERNAL_SERVER_ERROR, "N506", "There is no enough snowflakes")
     ;
 
     private final HttpStatus httpStatus;
