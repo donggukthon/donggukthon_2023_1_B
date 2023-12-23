a431c94a7a41b5956713c47828661f19e30cc0e2
nykoh2001 <nykoh2001@gmail.com>
2023-12-24 01:51:41 +0900

    feat: static factory method (ok, created)

--- src/main/java/flirting/demo/dto/common/ResponseDto.java
+++ src/main/java/flirting/demo/dto/common/ResponseDto.java
@@ -22,4 +22,19 @@ public class ResponseDto<T> {
         this.exception = exception;
     }
 
+    // Response에서 사용되는 일반적인 응답에 대한 메소드 정의
+    // static factory method
+    // factory method로 객체를 생성해서 반환해 주고 있음 -> 객체 생성 메소드는 클래스에만 정의되는 것으로 충분
+    // 인스턴스마다 팩토리 메소드가 정의되어 있는 것은 불필요
+
+    // OK
+    public static <T> ResponseDto<T> ok(@Nullable final T data){
+        return new ResponseDto<>(HttpStatus.OK, true, data, null);
+    }
+
+    // CREATED
+    public static <T> ResponseDto<T> created(@Nullable final T data){
+        return new ResponseDto<>(HttpStatus.CREATED, true, data, null);
+    }
+
 }
