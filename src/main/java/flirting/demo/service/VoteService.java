d52252fcd8eba6931ed0313a3aaf437c5087a472
nykoh2001 <nykoh2001@gmail.com>
2023-12-26 02:44:45 +0900

    fix: vote domain 예외 처리

--- src/main/java/flirting/demo/service/VoteService.java
+++ src/main/java/flirting/demo/service/VoteService.java
@@ -1,13 +1,13 @@
 package flirting.demo.service;
 
-import flirting.demo.dto.common.CustomException;
-import flirting.demo.dto.common.StatusCode;
 import flirting.demo.dto.request.VoteGuessRequest;
 import flirting.demo.dto.request.VoteRequest;
 import flirting.demo.entity.Group;
 import flirting.demo.entity.Member;
 import flirting.demo.entity.Question;
 import flirting.demo.entity.Vote;
+import flirting.demo.exception.CommonException;
+import flirting.demo.exception.ErrorCode;
 import flirting.demo.repository.GroupRepository;
 import flirting.demo.repository.MemberRepository;
 import flirting.demo.repository.QuestionRepository;
@@ -28,19 +28,36 @@ public class VoteService {
     private final MemberRepository memberRepository;
     private final GroupRepository groupRepository;
 
-    public Vote createVote(VoteRequest voteRequest){
-        Question question = questionRepository.getReferenceById(voteRequest.getQuestionId());
-        Member voter = memberRepository.getReferenceById(voteRequest.getVoterId());
-        Member selectedMember = memberRepository.getReferenceById(voteRequest.getSelectedMemberId());
-        Group group = groupRepository.getReferenceById(voteRequest.getGroupId());
+    public Vote createVote(VoteRequest voteRequest) {
+        Long questionId = voteRequest.getQuestionId();
+        if (!questionRepository.existsById(questionId)) throw new CommonException(ErrorCode.QUESTION_NOT_FOUND);
+        Question question = questionRepository.getReferenceById(questionId);
+
+        Long voterId = voteRequest.getVoterId();
+        if (!memberRepository.existsById(voterId)) throw new CommonException(ErrorCode.MEMBER_NOT_FOUND);
+        Member voter = memberRepository.getReferenceById(voterId);
+
+        Long selectedMemberId = voteRequest.getSelectedMemberId();
+        if (!memberRepository.existsById(selectedMemberId)) throw new CommonException(ErrorCode.MEMBER_NOT_FOUND);
+        Member selectedMember = memberRepository.getReferenceById(selectedMemberId);
+
+        Long groupId = voteRequest.getGroupId();
+        if (!groupRepository.existsById(groupId)) throw new CommonException(ErrorCode.GROUP_NOT_FOUND);
+        Group group = groupRepository.getReferenceById(groupId);
+
         Vote vote = voteRequest.toEntity(question, voter, selectedMember, group);
         Vote _vote = voteRepository.save(vote);
+
         return _vote;
     }
 
     public VoteResult getVoteResult(Long memberId, Long groupId, Long questionId) {
+        if (!memberRepository.existsById(memberId)) throw new CommonException(ErrorCode.MEMBER_NOT_FOUND);
+        if (!groupRepository.existsById(groupId)) throw new CommonException(ErrorCode.GROUP_NOT_FOUND);
+        if (!questionRepository.existsById(questionId)) throw new CommonException(ErrorCode.QUESTION_NOT_FOUND);
+
         List<Member> voters = voteRepository.getMostVoted(groupId, questionId);
-        Member mostVoted = voters.stream().findFirst().get();
+        Member mostVoted = voters.stream().findFirst().orElseThrow(() -> new CommonException(ErrorCode.VOTE_NOT_FOUND));
 
         Long mostVotedCnt = voteRepository.getMostVotedCnt(mostVoted.getId(), groupId, questionId);
 
@@ -57,23 +74,32 @@ public class VoteService {
     }
 
     public Long getTotalVoteCnt(Long groupId, Long questionId) {
+        if (!groupRepository.existsById(groupId)) throw new CommonException(ErrorCode.GROUP_NOT_FOUND);
+        if (!questionRepository.existsById(questionId)) throw new CommonException(ErrorCode.QUESTION_NOT_FOUND);
+
         return voteRepository.getTotalVoteCnt(groupId, questionId);
     }
 
     public Question getCurrentQuestion(Long questionId) {
+        if (!questionRepository.existsById(questionId)) throw new CommonException(ErrorCode.QUESTION_NOT_FOUND);
+
         return questionRepository.getReferenceById(questionId);
     }
 
     public List<Member> getOptionList(Long memberId, Long groupId) {
+        if (!memberRepository.existsById(memberId)) throw new CommonException(ErrorCode.MEMBER_NOT_FOUND);
+        if (!groupRepository.existsById(groupId)) throw new CommonException(ErrorCode.GROUP_NOT_FOUND);
 
         List<Member> options = memberRepository.getAllMembersExceptMe(memberId, groupId);
         if (options.stream().filter(op -> op.getId() == memberId).findAny().isPresent()) {
-            throw new CustomException(StatusCode.MYSELF_IN_OPTIONS);
+            throw new CommonException(ErrorCode.MYSELF_IN_OPTION);
         }
         return options;
     }
 
-    public Integer getSnowFlakes(Long memberId){
+    public Integer getSnowFlakes(Long memberId) {
+        if (!memberRepository.existsById(memberId)) throw new CommonException(ErrorCode.MEMBER_NOT_FOUND);
+
         Member member = memberRepository.getReferenceById(memberId);
         return member.getSnowflake();
     }
@@ -84,35 +110,45 @@ public class VoteService {
         Long questionId = voteGuessRequest.getQuestionId();
         Long groupId = voteGuessRequest.getGroupId();
 
+        if (!memberRepository.existsById(memberId)) throw new CommonException(ErrorCode.MEMBER_NOT_FOUND);
+
         Member member = memberRepository.getReferenceById(memberId);
 
-        if (member.getSnowflake() < 10){
-            throw new CustomException(StatusCode.SNOWFLAKE_NOT_ENOUGH);
+        if (member.getSnowflake() < 10) {
+            throw new CommonException(ErrorCode.SNOWFLAKES_NOT_ENOUGH);
         }
+
+
+        if (!memberRepository.existsById(selectedMemberId)) throw new CommonException(ErrorCode.MEMBER_NOT_FOUND);
+        if (!groupRepository.existsById(groupId)) throw new CommonException(ErrorCode.GROUP_NOT_FOUND);
+        if (!questionRepository.existsById(questionId)) throw new CommonException(ErrorCode.QUESTION_NOT_FOUND);
+
         Optional<Vote> vote = voteRepository.getVoteByGuessRequest(memberId, selectedMemberId, groupId, questionId);
 
         if (vote.isEmpty()) {
             return false;
-        } else if (vote.isPresent()) {
+        } else {
             member.updateSnowflake(+15);
             memberRepository.save(member);
             return true;
-        } else {
-            throw new CustomException(StatusCode.INTERNAL_SERVER_ERROR);
         }
     }
 
     public Member updateSnowFlakes(Long memberId) {
+        if (!memberRepository.existsById(memberId)) throw new CommonException(ErrorCode.MEMBER_NOT_FOUND);
+
         Member member = memberRepository.getReferenceById(memberId);
-        if (member.getSnowflake() < 10){
-            throw new CustomException(StatusCode.SNOWFLAKE_NOT_ENOUGH);
+        if (member.getSnowflake() < 10) {
+            throw new CommonException(ErrorCode.SNOWFLAKES_NOT_ENOUGH);
         }
+
         member.updateSnowflake(-10);
         Member _member = memberRepository.save(member);
         return _member;
     }
 
     public Member getMemberById(Long memberId) {
+        if (!memberRepository.existsById(memberId)) throw new CommonException(ErrorCode.MEMBER_NOT_FOUND);
         return memberRepository.getReferenceById(memberId);
     }
 
@@ -129,6 +165,7 @@ public class VoteService {
     }
 
     public Long getMemberCnt(Long groupId) {
+        if (!groupRepository.existsById(groupId)) throw new CommonException(ErrorCode.GROUP_NOT_FOUND);
         return memberRepository.getMemberCnt(groupId);
     }
 
@@ -140,7 +177,7 @@ public class VoteService {
         Integer myVoteCnt;
 
         @Builder
-        public VoteResult(String mostVoted,Integer mostVotedCnt, String memberName, Integer myVoteCnt) {
+        public VoteResult(String mostVoted, Integer mostVotedCnt, String memberName, Integer myVoteCnt) {
             this.mostVoted = mostVoted;
             this.mostVotedCnt = mostVotedCnt;
             this.memberName = memberName;
