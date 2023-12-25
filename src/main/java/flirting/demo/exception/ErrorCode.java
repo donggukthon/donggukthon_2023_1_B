5197ddd10106ff1dcc5a7b2a994c0d307e975829
nykoh2001 <nykoh2001@gmail.com>
2023-12-26 02:14:41 +0900

    feat: question domain error

--- src/main/java/flirting/demo/exception/ErrorCode.java
+++ src/main/java/flirting/demo/exception/ErrorCode.java
@@ -17,10 +17,12 @@ public enum ErrorCode {
 
     MEMBER_NOT_FOUND(4040, HttpStatus.NOT_FOUND, "조회된 멤버 없음"),
     GROUP_NOT_FOUND(4041, HttpStatus.NOT_FOUND, "조회된 그룹 없음"),
+    QUESTION_NOT_FOUND(4042, HttpStatus.NOT_FOUND, "조회된 질문 없음"),
 
 
     INTERNAL_SERVER_ERROR(5000, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러 발생"),
     GROUP_ALREADY_JOINED(5001, HttpStatus.INTERNAL_SERVER_ERROR, "이미 가입된 그룹"),
+    QUESTION_CNT_NOT_SEVEN(5002, HttpStatus.INTERNAL_SERVER_ERROR, "질문이 7개가 아닙니다."),
     ;
 
     private final Integer code;
