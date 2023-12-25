d95e752501f71deedfa9dd3f5b1fc1fcd585d28d
nykoh2001 <nykoh2001@gmail.com>
2023-12-26 01:59:39 +0900

    feat: 에러 코드 추가

--- src/main/java/flirting/demo/exception/ErrorCode.java
+++ src/main/java/flirting/demo/exception/ErrorCode.java
@@ -8,10 +8,19 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    INVALID_ARGUMENT(400, HttpStatus.OK.BAD_REQUEST, "유효하지 않은 인자"),
-    MISSING_REQUEST_PARAMETER(401, HttpStatus.BAD_REQUEST, "필수 파라미터 누락"),
-    INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러 발생"),
-    ACCESS_DENIED(403, HttpStatus.INTERNAL_SERVER_ERROR, "접근 권한 거부")
+    INVALID_ARGUMENT(4000, HttpStatus.OK.BAD_REQUEST, "유효하지 않은 인자"),
+    MISSING_REQUEST_PARAMETER(4001, HttpStatus.BAD_REQUEST, "필수 파라미터 누락"),
+
+
+    ACCESS_DENIED(4030, HttpStatus.INTERNAL_SERVER_ERROR, "접근 권한 거부"),
+
+
+    MEMBER_NOT_FOUND(4040, HttpStatus.NOT_FOUND, "조회된 멤버 없음"),
+    GROUP_NOT_FOUND(4041, HttpStatus.NOT_FOUND, "조회된 그룹 없음"),
+
+
+    INTERNAL_SERVER_ERROR(5000, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러 발생"),
+    GROUP_ALREADY_JOINED(5001, HttpStatus.INTERNAL_SERVER_ERROR, "이미 가입된 그룹"),
     ;
 
     private final Integer code;
